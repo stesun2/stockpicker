7aa16d3385cd6b981e8d636eb2dca5ca6bcef5fe
stesun2 <ss27@illinois.edu>
2021-06-03 15:49:16 -0500

    work in progress

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,51 @@
 def picker(prices):
-    pass
+    # PSEUDOCODE
+    # Iterate through a list, return a pair of indices with the greatest difference 
+    # of their respective values with the lower value with lower index 
+    # e.g. [3,6,6,3,15] lowest index 0, highest index 4
+    # Skip index 0 when searching for high value(sell)
+
+# You need to buy before you can sell
+# Pay attention to edge cases like when the lowest day is the last day or the highest day is the first day.# 
+    
+    low      = 0
+    high     = 0
+    low_idx  = 0
+    high_idx = 0
+    price    = 0
+    buy_sell = []
+    blah = []
+
+  
+    for day, price in enumerate(prices):
+        # buy first finding lowest price, get status [searching, bought, maybe, sold] 
+        if price > high and day >= 1:
+            high = price
+            high_idx = day
+            # sell
+            for day, price in enumerate(prices[0:high]):
+
+                if price <= low:
+                    low = price
+                print('prices:', prices[0:high]) 
+
+        # if price > high:
+        #     high = price
+    # print('price:', price)       
+    # print('high:', high)
+        # else:
+        #     continue
+        # another for loop
+        # for price in range(0, high):
+        #     if price <= low:
+        #         low = price
+        #         print('high:', high, 'low:',low)
+        # print('day:',day, 'price: $',price)
+    # print('low:', low)       
+    # print('high:', high)   
+
+    return prices
+
+# print(picker([8,3,6,4,1]))
+# print(picker([6,4,1]))
+print(picker([17,3,6,9,15,8,6,1]))
--- python/stock_picker_spec.py
+++ python/stock_picker_spec.py
@@ -1,8 +1,15 @@
 # Rewrite this in Unit Test
-
+import unittest
 from stock_picker import picker
 
-print(picker([17,3,6,9,15,8,6,1,10]) == [1,4])
-print(picker([3,17,6,9,18,15,6,1,10]) == [0,4])
-print(picker([1,17,6,9,8,15,6,3,19]) == [0,8])
-print(picker([19,17,6,9,8,15,6,3,1]) == [2,5])
+class StockPickerTest(unittest.TestCase):
+    # def 
+
+# print(picker([17,3,6,9,15,8,6,1,10]) == [1,4])  $15 - $3 = $12
+# print(picker([3,17,6,9,18,15,6,1,10]) == [0,4]) $18 - $3 = $15
+# print(picker([1,17,6,9,8,15,6,3,19]) == [0,8])  $19 - $1 = $18
+# print(picker([19,17,6,9,8,15,6,3,1]) == [2,5])  $15 - $6 = $9
+
+
+if __name__ == '__main__':
+    unittest.main()
